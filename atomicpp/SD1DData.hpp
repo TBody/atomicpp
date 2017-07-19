a34c63f73e166825103ca74150eccd2d7c8d2d8c
Tom Body <thomas.body@outlook.com>
2017-07-19 08:40:12 +0100

    Basic SD1D working
    Performing 1D extract and setting of impurity density immediately, since the C++ code will be implemented at a single timestep

--- atomicpp/SD1DData.hpp
+++ atomicpp/SD1DData.hpp
@@ -14,16 +14,13 @@
 		// # For storing the data output from SD1D. To create the required JSON run the function
 		// # data_dict_export.py in an I/O (case) folder in SD1D.
 		public:
-			SD1DData(const string& input_file);
-			void setImpurityFraction(float impurity_fraction);
-			void setImpurityDensity(float impurity_density);
-			void selectSingleTime(float t);
+			SD1DData(const string& input_file, double impurity_fraction);
 		private:
 			vector<double> temperature;
 			vector<double> density;
 			vector<double> neutral_fraction;
-			vector<double> impurity_density;
-			double data_shape[2];
 			double impurity_fraction;
+			vector<double> impurity_density;
+			// double data_shape[2];
 		};
 #endif
