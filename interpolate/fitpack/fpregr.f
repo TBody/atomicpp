7dd79405dd41c5e17d9777682b31e229eb7a0de6
Tom Body <thomas.body@outlook.com>
2017-08-16 23:59:28 +0100

    Added in branch checking for program

--- interpolate/fitpack/fpregr.f
+++ interpolate/fitpack/fpregr.f
@@ -68,6 +68,7 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c  determine the number of knots for polynomial approximation.
       nminx = 2*kx1
       nminy = 2*ky1
+      if(iopt.lt.0) print *, "BA_FPEGR 1"      
       if(iopt.lt.0) go to 120
 c  acc denotes the absolute tolerance for the root of f(p)=s.
       acc = tol*s
@@ -79,19 +80,23 @@ c  find nxe and nye which denote the maximum number of knots
 c  allowed in each direction
       nxe = min0(nmaxx,nxest)
       nye = min0(nmaxy,nyest)
+      if(s.gt.0.) print *, "BA_FPEGR 2"      
       if(s.gt.0.) go to 100
 c  if s = 0, s(x,y) is an interpolating spline.
       nx = nmaxx
       ny = nmaxy
 c  test whether the required storage space exceeds the available one.
+      if(ny.gt.nyest .or. nx.gt.nxest) print *, "BA_FPEGR 3"
       if(ny.gt.nyest .or. nx.gt.nxest) go to 420
 c  find the position of the interior knots in case of interpolation.
 c  the knots in the x-direction.
       mk1 = mx-kx1
+      if(mk1.eq.0)print *, "BA_FPEGR 4"      
       if(mk1.eq.0) go to 60
       k3 = kx/2
       i = kx1+1
       j = k3+2
+      if(k3*2.eq.kx)print *, "BA_FPEGR 5"      
       if(k3*2.eq.kx) go to 40
       do 30 l=1,mk1
         tx(i) = x(j)
@@ -106,10 +111,12 @@ c  the knots in the x-direction.
   50  continue
 c  the knots in the y-direction.
   60  mk1 = my-ky1
+      if(mk1.eq.0) print *, "BA_FPEGR 6"  
       if(mk1.eq.0) go to 120
       k3 = ky/2
       i = ky1+1
       j = k3+2
+      if(k3*2.eq.ky)print *, "BA_FPEGR 7"      
       if(k3*2.eq.ky) go to 80
       do 70 l=1,mk1
         ty(i) = y(j)
@@ -124,7 +131,9 @@ c  the knots in the y-direction.
   90  continue
       go to 120
 c  if s > 0 our initial choice of knots depends on the value of iopt.
+      if(iopt.eq.0) print *, "BA_FPEGR 8"
  100  if(iopt.eq.0) go to 115
+      if(fp0.le.s) print *, "BA_FPEGR 9" 
       if(fp0.le.s) go to 115
 c  if iopt=1 and fp0 > s we start computing the least- squares spline
 c  according to the set of knots found at the last call of the routine.
@@ -136,6 +145,7 @@ c  interval (tx(l),tx(l+1)).
       mpm = mx-1
       do 105 i=2,mpm
         nrdatx(j) = nrdatx(j)+1
+      if(x(i).lt.tx(l)) print *, "BA_FPEGR 10"        
         if(x(i).lt.tx(l)) go to 105
         nrdatx(j) = nrdatx(j)-1
         l = l+1
@@ -150,6 +160,7 @@ c  interval (ty(l),ty(l+1)).
       mpm = my-1
       do 110 i=2,mpm
         nrdaty(j) = nrdaty(j)+1
+      if(y(i).lt.ty(l)) print *, "BA_FPEGR 11"        
         if(y(i).lt.ty(l)) go to 110
         nrdaty(j) = nrdaty(j)-1
         l = l+1
@@ -180,6 +191,7 @@ c  interior knots).
 c  main loop for the different sets of knots.mpm=mx+my is a save upper
 c  bound for the number of trials.
       do 250 iter=1,mpm
+        if(nx.eq.nminx .and. ny.eq.nminy)print *, "BA_FPEGR 12"        
         if(nx.eq.nminx .and. ny.eq.nminy) ier = -2
 c  find nrintx (nrinty) which is the number of knot intervals in the
 c  x-direction (y-direction).
@@ -214,24 +226,32 @@ c  fp gives the total sum of squared residuals.
      *  ny,p,c,nc,fp,fpintx,fpinty,mm,mynx,kx1,kx2,ky1,ky2,wrk(lsx),
      *  wrk(lsy),wrk(lri),wrk(lq),wrk(lax),wrk(lay),wrk(lbx),wrk(lby),
      *  nrx,nry)
+        if(ier.eq.(-2))print *, "BA_FPEGR 13"        
         if(ier.eq.(-2)) fp0 = fp
 c  test whether the least-squares spline is an acceptable solution.
+        if(iopt.lt.0) print *, "BA_FPEGR 14"
         if(iopt.lt.0) go to 440
         fpms = fp-s
+        if(abs(fpms) .lt. acc) print *, "BA_FPEGR 15"        
         if(abs(fpms) .lt. acc) go to 440
 c  if f(p=inf) < s, we accept the choice of knots.
+        if(fpms.lt.0.) print *, "BA_FPEGR 16"
         if(fpms.lt.0.) go to 300
 c  if nx=nmaxx and ny=nmaxy, sinf(x,y) is an interpolating spline.
+        if(nx.eq.nmaxx .and. ny.eq.nmaxy) print *, "BA_FPEGR 17"
         if(nx.eq.nmaxx .and. ny.eq.nmaxy) go to 430
 c  increase the number of knots.
 c  if nx=nxe and ny=nye we cannot further increase the number of knots
 c  because of the storage capacity limitation.
+        if(nx.eq.nxe .and. ny.eq.nye) print *, "BA_FPEGR 18"
         if(nx.eq.nxe .and. ny.eq.nye) go to 420
         ier = 0
 c  adjust the parameter reducx or reducy according to the direction
 c  in which the last added knots were located.
-        if (lastdi.lt.0) go to 150
-        if (lastdi.eq.0) go to 170
+        if(lastdi.lt.0) print *, "BA_FPEGR 18.1"
+        if(lastdi.lt.0) go to 150
+        if(lastdi.eq.0) print *, "BA_FPEGR 18.2"
+        if(lastdi.eq.0) go to 170
         go to 160
  150    reducx = fpold-fp
         go to 170
@@ -240,22 +260,30 @@ c  store the sum of squared residuals for the current set of knots.
  170    fpold = fp
 c  find nplx, the number of knots we should add in the x-direction.
         nplx = 1
+        if(nx.eq.nminx) print *, "BA_FPEGR 19"        
         if(nx.eq.nminx) go to 180
         npl1 = nplusx*2
         rn = nplusx
+        if(reducx.gt.acc) print *, "BA_FPEGR 20"        
         if(reducx.gt.acc) npl1 = rn*fpms/reducx
         nplx = min0(nplusx*2,max0(npl1,nplusx/2,1))
 c  find nply, the number of knots we should add in the y-direction.
  180    nply = 1
+        if(ny.eq.nminy) print *, "BA_FPEGR 21" 
         if(ny.eq.nminy) go to 190
         npl1 = nplusy*2
         rn = nplusy
+        if(reducy.gt.acc) print *, "BA_FPEGR 22"        
         if(reducy.gt.acc) npl1 = rn*fpms/reducy
         nply = min0(nplusy*2,max0(npl1,nplusy/2,1))
- 190    if (nplx.lt.nply) go to 210
-        if (nplx.eq.nply) go to 200
+ 190    if(nplx.lt.nply) print *, "BA_FPEGR 18.3"
+ 190    if(nplx.lt.nply) go to 210
+        if(nplx.eq.nply) print *, "BA_FPEGR 18.4"
+        if(nplx.eq.nply) go to 200
         go to 230
+        if(lastdi.lt.0) print *, "BA_FPEGR 23"        
  200    if(lastdi.lt.0) go to 230
+        if(nx.eq.nxe) print *, "BA_FPEGR 24" 
  210    if(nx.eq.nxe) go to 230
 c  addition in the x-direction.
         lastdi = -1
@@ -266,9 +294,11 @@ c  add a new knot in the x-direction
           call fpknot(x,mx,tx,nx,fpintx,nrdatx,nrintx,nxest,1)
 c  test whether we cannot further increase the number of knots in the
 c  x-direction.
+          if(nx.eq.nxe) print *, "BA_FPEGR 25"
           if(nx.eq.nxe) go to 250
  220    continue
         go to 250
+        if(ny.eq.nye) print *, "BA_FPEGR 26"        
  230    if(ny.eq.nye) go to 210
 c  addition in the y-direction.
         lastdi = 1
@@ -279,12 +309,14 @@ c  add a new knot in the y-direction.
           call fpknot(y,my,ty,ny,fpinty,nrdaty,nrinty,nyest,1)
 c  test whether we cannot further increase the number of knots in the
 c  y-direction.
+          if(ny.eq.nye) print *, "BA_FPEGR 27"
           if(ny.eq.nye) go to 250
  240    continue
 c  restart the computations with the new set of knots.
  250  continue
 c  test whether the least-squares polynomial is a solution of our
 c  approximation problem.
+      if(ier.eq.(-2)) print *, "BA_FPEGR 28"
  300  if(ier.eq.(-2)) go to 440
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c part 2: determination of the smoothing spline sp(x,y)                c
@@ -321,34 +353,46 @@ c  squared residuals fp.
      *  nrx,nry)
 c  test whether the approximation sp(x,y) is an acceptable solution.
         fpms = fp-s
+        if(abs(fpms).lt.acc) print *, "BA_FPEGR 29"        
         if(abs(fpms).lt.acc) go to 440
 c  test whether the maximum allowable number of iterations has been
 c  reached.
+        if(iter.eq.maxit) print *, "BA_FPEGR 30"
         if(iter.eq.maxit) go to 400
 c  carry out one more step of the iteration process.
         p2 = p
         f2 = fpms
+        if(ich3.ne.0) print *, "BA_FPEGR 31"        
         if(ich3.ne.0) go to 320
+        if((f2-f3).gt.acc) print *, "BA_FPEGR 32"        
         if((f2-f3).gt.acc) go to 310
 c  our initial choice of p is too large.
         p3 = p2
         f3 = f2
         p = p*con4
+        if(p.le.p1) print *, "BA_FPEGR 33"        
         if(p.le.p1) p = p1*con9 + p2*con1
         go to 350
+        if(f2.lt.0.)print *, "BA_FPEGR 34"        
  310    if(f2.lt.0.) ich3 = 1
+        if(ich1.ne.0) print *, "BA_FPEGR 35" 
  320    if(ich1.ne.0) go to 340
+        if((f1-f2).gt.acc) print *, "BA_FPEGR 36" 
         if((f1-f2).gt.acc) go to 330
 c  our initial choice of p is too small
         p1 = p2
         f1 = f2
         p = p/con4
+        if(p3.lt.0.) print *, "BA_FPEGR 37"        
         if(p3.lt.0.) go to 350
+        if(p.ge.p3) print *, "BA_FPEGR 38"        
         if(p.ge.p3) p = p2*con1 + p3*con9
         go to 350
 c  test whether the iteration process proceeds as theoretically
 c  expected.
+        if(f2.gt.0.)print *, "BA_FPEGR 39"
  330    if(f2.gt.0.) ich1 = 1
+        if(f2.ge.f1 .or. f2.le.f3) print *, "BA_FPEGR 40" 
  340    if(f2.ge.f1 .or. f2.le.f3) go to 410
 c  find the new value of p.
         p = fprati(p1,f1,p2,f2,p3,f3)
