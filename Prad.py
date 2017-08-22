45c85bef01d2b2f1ee7534113871fecb99f15aa8
Tom Body <thomas.body@outlook.com>
2017-08-22 19:24:36 +0100

    Pushed AtomicSolver to class
    Following https://stackoverflow.com/questions/16904202/numpy-odeint-output-extra-variables to try to be able to export the radiated power as a function of time

--- Prad.py
+++ Prad.py
@@ -7,102 +7,146 @@
 
 import numpy as np
 from atomicpp import atomicpy
-
-impurity_symbol = b'c' #need to include b (bytes) before the string for it to be sent as a std::string to C++
-
-impurity = atomicpy.PyImpuritySpecies(impurity_symbol)
-
-impurity_derivatives = atomicpy.PyRateEquations(impurity);
-impurity_derivatives.setThresholdDensity(0.0);
-impurity_derivatives.setDominantIonMass(1.0);
-
-Te = 50; #eV
-Ne = 1e19; #m^-3
-Vi = 0; #m/s
-Nn = 0; #m^-3
-Vn = 0; #m/s
-
-Z=impurity.get_atomic_number()
-
-# From collisional radiative equilibrium, start values for the Carbon impurity densities
-Nzk_init = np.array([1.747803e-01, 1.366167e+05, 8.865589e+09, 6.294431e+13, 9.049412e+16, 9.440710e+15, 3.206463e+13])
-# Don't treat momentum yet
-Vzk = np.zeros((Z+1,))
-
 from scipy.integrate import odeint
-from scipy.optimize import fsolve
-# Two different methods for finding stable values
 
-def evolve_density_TD(Nzk, t, Te, Ne, Vi, Nn, Vn, Vzk):
-	# Time-dependant, for odeint
+class AtomicSolver(object):
 
-	# Prevent negative densities
-	# (these are possible if the time-step is reasonably large)
-	# for k in range(len(Nzk)):
-	# 	if(Nzk[k] < 0):
-	# 		Nzk[k] = 0
+	def __init__(self, impurity_symbol):
 
-	derivative_struct = impurity_derivatives.computeDerivs(Te, Ne, Vi, Nn, Vn, Nzk, Vzk);
+		self.impurity = atomicpy.PyImpuritySpecies(impurity_symbol)
 
-	dNzk = derivative_struct["dNzk"]
+		self.Z = self.impurity.get_atomic_number()
 
-	return derivative_struct["dNzk"]
+		self.impurity_derivatives = atomicpy.PyRateEquations(self.impurity)
+		self.impurity_derivatives.setThresholdDensity(-1.0) #Don't use a threshold density at first
+		self.impurity_derivatives.setDominantIonMass(1.0)
+
+		self.Te  = 50 #eV
+		self.Ne  = 1e19 #m^-3
+		self.Vi  = 0 #m/s
+		self.Nn  = 0 #m^-3
+		self.Vn  = 0 #m/s
+		self.Nzk = np.array([1e17, 0, 0, 0, 0, 0, 0]) #m^-3
+		self.Vzk = np.zeros((self.Z+1,)) #m/s
+
+	@staticmethod
+	def evolveDensity(Nzk, t, self):
+		Te  = self.Te
+		Ne  = self.Ne
+		Vi  = self.Vi
+		Nn  = self.Nn
+		Vn  = self.Vn
+		# Nzk = self.Nzk
+		Vzk = self.Vzk
+
+		# Prevent negative densities
+		# (these are possible if the time-step is reasonably large)
+		# for k in range(len(Nzk)):
+		# 	if(Nzk[k] < 0):
+		# 		Nzk[k] = 0
+
+		derivative_struct = self.impurity_derivatives.computeDerivs(Te, Ne, Vi, Nn, Vn, Nzk, Vzk);
+
+		dNzk = derivative_struct["dNzk"]
+
+		return dNzk
+
+	def solveCollisionalRadiativeEquilibrium(self, Te, Ne, t_values = np.logspace(-6, 2, 200)):
+		Vi  = self.Vi
+		Nn  = self.Nn
+		Vn  = self.Vn
+		Vzk = self.Vzk
+
+		print("Te = {}eV, Ne = {}/m3".format(Te, Ne))
+		
+		result = odeint(self.evolveDensity, self.Nzk, t_values, args=(self,), printmessg=False)
+
+		# for k in range(self.Z+1):
+		# 	print("{:20} Nz^{} = {}".format("Equilibrium found for",k,result[-1,k]))
+
+		return result
+
+	def scanCollisionalRadiativeEquilibrium(self, Te_values, Ne_values, t_values = np.logspace(-6, 2, 200)):
+
+		result_grid = np.zeros((self.Z+1, len(Te_values), len(Ne_values)))
+
+		for Te_iterator in range(len(Te_values)):
+			Te = Te_values[Te_iterator]
+			for Ne_iterator in range(len(Ne_values)):
+				Ne = Ne_values[Ne_iterator]
+
+				print("Evaluating test {} of {}".format(Te_iterator*len(Te_values)+Ne_iterator, len(Te_values)*len(Ne_values)))
+
+				result = self.solveCollisionalRadiativeEquilibrium(Te, Ne, t_values)
+
+				result_grid[:,Te_iterator,Ne_iterator] = result[-1,:]
+
+		return result_grid
+
+				
+
+
 
 if __name__ == "__main__":
 
 	import matplotlib.pyplot as plt
 
-	# Te = 1
+	impurity_symbol = b'c' #need to include b (bytes) before the string for it to be sent as a std::string to C++
 
-	# t = np.logspace(-6, 2, 200)
-	# result = odeint(evolve_density_TD, Nzk_init, t, args=(Te, Ne, Vi, Nn, Vn, Vzk), printmessg=True)
+	solver = AtomicSolver(impurity_symbol)
 
-	# for k in range(Z+1):
-	# 	plt.semilogx(t, result[:,k], label="{}".format(k))
-	# 	# print("Nz^{} = {}".format(k,result[-1,k]))
-	# plt.semilogx(t, np.sum(result[:,:],1), label="Total")
-	# plt.xlabel(r'Time (s)')
-	# plt.ylabel(r'Density of stage ($m^{-3}$)')
-	# plt.title('Time evolution of ionisation stages')
-	# plt.legend()
-	# plt.show()
+	# solver.Te = 50
+	# solver.Nzk = np.array([1e17, 0, 0, 0, 0, 0, 0])
 
+	t = np.logspace(-6, 2, 200)
+	result = solver.solveCollisionalRadiativeEquilibrium(50, 1e19, t)
 
-
-
-	Te_values = np.logspace(-0.69, 3.99, 100) #Span the entire array for which there is ADAS data
-	Ne_values = np.logspace(13.7, 21.3, 100)
-
-	t = np.logspace(-6, 2, 2000)
-
-	Ne = 1e19; #m^-3
-
-	Nzk_init = np.ones((Z+1,)) * 1e17/Z
-	# Nzk_init[0] = 1e17
-
-	Ionisation_stage_distibution = np.zeros((Z+1,len(Te_values)))
-
-	for Te_iterator in range(len(Te_values)):
-		Te = Te_values[Te_iterator]
-
-		result = odeint(evolve_density_TD, Nzk_init, t, args=(Te, Ne, Vi, Nn, Vn, Vzk))
-
-		# for k in range(Z+1):
-			# print("Nz^{} = {}".format(k,result[-1,k]))
-
-		Ionisation_stage_distibution[:,Te_iterator] = result[-1,:]
-		# Nzk_init = result[-1,:]
-
-	for k in range(Z+1):
-		plt.loglog(Te_values, Ionisation_stage_distibution[k,:], label="{}".format(k))
-	
-	plt.loglog(Te_values, np.sum(Ionisation_stage_distibution[:,:],0), label="Total")
-	plt.ylim([1e-3*np.sum(Ionisation_stage_distibution[:,-1],0), 1*np.sum(Ionisation_stage_distibution[:,-1],0)])
-	plt.xlabel(r'Plasma temperature (eV)')
+	for k in range(solver.Z+1):
+		plt.semilogx(t, result[:,k], label="{}".format(k))
+		
+	plt.semilogx(t, np.sum(result[:,:],1), label="Total")
+	plt.xlabel(r'Time (s)')
 	plt.ylabel(r'Density of stage ($m^{-3}$)')
-	plt.title(r'Ionisation stage at C.R. as $f(T_e)$')
+	plt.title('Time evolution of ionisation stages')
 	plt.legend()
 	plt.show()
+
+
+
+
+	# Te_values = np.logspace(-0.69, 3.99, 100) #Span the entire array for which there is ADAS data
+	# Ne_values = np.logspace(13.7, 21.3, 100)
+
+	# t = np.logspace(-6, 2, 2000)
+
+	# Ne = 1e19; #m^-3
+
+	# Nzk_init = np.ones((Z+1,)) * 1e17/Z
+	# # Nzk_init[0] = 1e17
+
+	# Ionisation_stage_distibution = np.zeros((Z+1,len(Te_values)))
+
+	# for Te_iterator in range(len(Te_values)):
+	# 	Te = Te_values[Te_iterator]
+
+	# 	result = odeint(evolve_density_TD, Nzk_init, t, args=(Te, Ne, Vi, Nn, Vn, Vzk))
+
+	# 	# for k in range(Z+1):
+	# 		# print("Nz^{} = {}".format(k,result[-1,k]))
+
+	# 	Ionisation_stage_distibution[:,Te_iterator] = result[-1,:]
+	# 	# Nzk_init = result[-1,:]
+
+	# for k in range(Z+1):
+	# 	plt.loglog(Te_values, Ionisation_stage_distibution[k,:], label="{}".format(k))
+
+	# plt.loglog(Te_values, np.sum(Ionisation_stage_distibution[:,:],0), label="Total")
+	# plt.ylim([1e-3*np.sum(Ionisation_stage_distibution[:,-1],0), 1*np.sum(Ionisation_stage_distibution[:,-1],0)])
+	# plt.xlabel(r'Plasma temperature (eV)')
+	# plt.ylabel(r'Density of stage ($m^{-3}$)')
+	# plt.title(r'Ionisation stage at C.R. as $f(T_e)$')
+	# plt.legend()
+	# plt.show()
 
 
 
